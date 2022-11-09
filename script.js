f3a0115eeaa8816d4e15ea72ba24d7b0ad18434f
LucioFurnari <luciofurnari99@gmail.com>
2022-11-09 18:51:50 -0300

    Add logic module and display module

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-let Gameboard = {
-    gameboard: ["","","",
+const gameBoard = (function() {
+    let board = ["","","",
                 "","","",
-                "","",""],
-};
+                "","",""];
+    function addMarker(event) {
+        board.map((elem,i) => {
+        if(this.getAttribute("number") == i){
+            if(board[i] == "X") {
+                console.log("Ya tiene x");
+                return
+            } else {
+                board[i] = "X";
+                console.log(board);
+            }
+        }
+    })
+    }
+    return {board, addMarker}
+})()
 
+
+const displayController = (function() {
+    const gameboardGrid = document.querySelector(".gameboard");
+    function displayMarker() {
+
+    }
+    function displayBoard (board) {
+        board.map((elem,i) => {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("number",i);
+            square.addEventListener("click",gameBoard.addMarker )
+            gameboardGrid.appendChild(square);
+        })
+    }
+    return {displayBoard}
+})()
+
+displayController.displayBoard(gameBoard.board)
